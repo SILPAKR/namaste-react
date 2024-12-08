481fb07439bc320f964b0393079093b39fe17055
Silpa K R <silpaunni1997@gmail.com>
2024-12-08 06:04:51 +0530

    Episode-01

---
+++ app.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading1" }, "Heading1"),
+    React.createElement("h2", { id: "heading2" }, "Heading2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading1" }, "Heading1"),
+    React.createElement("h2", { id: "heading2" }, "Heading2"),
+  ]),
+]);
+//heading is a js object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
